898ddbee88dc668b7ee80169f5fba3cae4a3553e
Asya Haroyan <alharoyan@gmail.com>
2024-04-15 16:10:00 +0000

    Add login sign up page

--- main/settings.py
+++ main/settings.py
@@ -53,8 +53,8 @@ INSTALLED_APPS = [
 ]
 
 SITE_ID = 1
-# LOGIN_REDIRECT_URL = '/'
-# LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -137,6 +137,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
