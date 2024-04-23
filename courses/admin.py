96dc80f36f5bddc226603cc60281da8ecd6532bf
Asya Haroyan <alharoyan@gmail.com>
2024-04-23 09:05:44 +0000

    Create model for lesson

--- courses/admin.py
+++ courses/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Lesson
+from .models import Lesson, Course
 
 # Register your models here.
-
+admin.site.register(Course)
 admin.site.register(Lesson)
--- courses/models.py
+++ courses/models.py
@@ -19,17 +19,22 @@ class Course(models.Model):
         (MATHS, 'Maths'),
         (ENGLISH, 'English'),
     ]
+    DEFAULT_SUBJECT = PHYSICS
 
     teacher = models.CharField(max_length=200, unique=True)
+    course_subject = models.CharField(max_length=20, choices=SUBJECT_CHOICES, default=DEFAULT_SUBJECT)
+
+    def __str__(self):
+        return f'{self.teacher} - {self.get_course_subject_display()}'
 
 
 class Lesson(models.Model):
     
     title = models.CharField(max_length=100, unique=True)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, default=1)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     description = models.TextField()
     slug = models.SlugField(max_length=200, unique=True)
-    video_lesson = CloudinaryField('image', default='placeholder')
+    video_lesson = CloudinaryField('video', default='placeholder')
     created_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
