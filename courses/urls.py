e3fdef2e329fa020dd1a0400ae748900a95c0c39
Asya Haroyan <alharoyan@gmail.com>
2024-04-25 09:01:27 +0000

    Debug true

--- courses/urls.py
+++ courses/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.LessonList.as_view(), name="courses"),
+    path('', views.LessonList.as_view(), name='courses'),
 ]
