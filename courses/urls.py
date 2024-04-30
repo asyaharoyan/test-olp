41e785c78886d057709471bfc440f3f663e44a10
Asya Haroyan <alharoyan@gmail.com>
2024-04-30 18:29:41 +0000

    Add course detail page

--- courses/urls.py
+++ courses/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.LessonList.as_view(), name='courses'),
+    path('<slug:slug>/', views.course_detail, name='course_detail'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.views.generic import ListView
 from .models import Course, Lesson
@@ -10,3 +10,28 @@ class LessonList(ListView):
     template_name = 'courses/online_courses.html'
     context_object_name = 'lessons'
     paginate_by = 6
+
+
+def course_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Lesson.objects.filter(status=1)
+    lesson = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "courses/course_detail.html",
+        {"lesson": lesson},
+    )
+   
