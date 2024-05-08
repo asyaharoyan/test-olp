3c40656884a7a0dfc47086c4d587691f198747e5
Asya Haroyan <alharoyan@gmail.com>
2024-05-08 13:49:59 +0000

    Add edit functionality

--- courses/urls.py
+++ courses/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.LessonList.as_view(), name='courses'),
     path('<slug:slug>/', views.course_detail, name='course_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.views.generic import ListView
 from django.contrib import messages
+from django.http import HttpResponseRedirect
 from .models import Course, Lesson, Comment
 from .forms import CommentForm
 
@@ -58,3 +59,25 @@ def course_detail(request, slug):
             },
     )
    
+
+def comment_edit(request, slug, comment_id):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Lesson.objects.filter(status=1)
+        lesson = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.commenter == request.user:
+            comment = comment_form.save(commit=False)
+            comment.lesson = lesson
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('course_detail', args=[slug]))
